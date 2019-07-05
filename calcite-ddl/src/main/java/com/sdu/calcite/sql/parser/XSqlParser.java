1a7443416401849ce0b7b551086cc04e138009c8
zhanghan13 <zhanghan13@meituan.com>
2019-07-05 19:48:12 +0800

    Calcite Parser

--- calcite-ddl/src/main/java/com/sdu/calcite/sql/parser/XSqlParser.java
+++ calcite-ddl/src/main/java/com/sdu/calcite/sql/parser/XSqlParser.java
@@ -3,7 +3,11 @@ package com.sdu.calcite.sql.parser;
 import org.apache.calcite.config.Lex;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlNodeList;
+import org.apache.calcite.sql.parser.SqlAbstractParserImpl;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.parser.SqlParserImplFactory;
 
+import java.io.Reader;
 import java.io.StringReader;
 
 /**
@@ -11,18 +15,29 @@ import java.io.StringReader;
  * */
 public class XSqlParser {
 
-    public static SqlNodeList parse(String text) throws Exception {
-        try (StringReader in = new StringReader(text)) {
-            XSqlParserImpl parser = new XSqlParserImpl(in);
+    private static final SqlParser.Config config;
 
-            // back tick as the quote
-            parser.switchTo("BTID");
-            parser.setTabSize(1);
-            parser.setQuotedCasing(Lex.JAVA.quotedCasing);
-            parser.setUnquotedCasing(Lex.JAVA.unquotedCasing);
-            parser.setIdentifierMaxLength(128);
-            return parser.parseSqlStmtList();
+    static {
+        config = SqlParser.configBuilder()
+                .setParserFactory(new SqlParserImplFactoryImpl())
+                .build();
+    }
+
+
+    private static class SqlParserImplFactoryImpl implements SqlParserImplFactory {
+
+        private SqlParserImplFactoryImpl() {}
+
+        @Override
+        public SqlAbstractParserImpl getParser(Reader stream) {
+            return new XSqlParserImpl(stream);
         }
+
+    }
+
+    public static SqlNodeList parse(String text) throws Exception {
+        SqlParser sqlParser = SqlParser.create(text, config);
+        return sqlParser.parseStmtList();
     }
 
     public static SqlNode parseOne(String text) throws Exception {
